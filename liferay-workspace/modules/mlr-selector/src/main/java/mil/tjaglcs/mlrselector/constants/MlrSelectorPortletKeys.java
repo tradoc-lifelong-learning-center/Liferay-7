4b3a911e59d08aeb9a87de3285f3d116b19b4c3d
Mike Dean <mldean@users.noreply.github.com>
2019-08-20 14:50:59 -0400

    Adding placeholder for MLR selector

---
+++ liferay-workspace/modules/mlr-selector/src/main/java/mil/tjaglcs/mlrselector/constants/MlrSelectorPortletKeys.java
@@ -0,0 +1,11 @@
+package mil.tjaglcs.mlrselector.constants;
+
+/**
+ * @author Jag
+ */
+public class MlrSelectorPortletKeys {
+
+	public static final String MLRSELECTOR =
+		"mil_tjaglcs_mlrselector_MlrSelectorPortlet";
+
+}
